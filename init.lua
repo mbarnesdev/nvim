cc09740ab78a89103fec649cb88ac407c1955e71
mbarnesdev <martinbarnes.dev@gmail.com>
2024-03-02 13:16:42 +0000

    lsp

--- init.lua
+++ init.lua
@@ -14,7 +14,6 @@ vim.g.loaded_matchit = 1
 vim.g.loaded_matchparen = 1
 vim.g.loaded_logiPat = 1
 vim.g.loaded_rrhelper = 1
-
 vim.opt.nu = true
 vim.opt.relativenumber = true
 vim.opt.tabstop = 4
@@ -25,14 +24,14 @@ vim.opt.softtabstop = 4
 vim.opt.shiftwidth = 4
 vim.opt.expandtab = true
 vim.opt.backup = false
-vim.opt.undodir = os.getenv("HOME") .. "/.vim/undodir"
+vim.opt.undodir = os.getenv "HOME" .. "/.vim/undodir"
 vim.opt.undofile = true
 vim.opt.hlsearch = false
 vim.opt.incsearch = true
 vim.opt.termguicolors = true
 vim.opt.scrolloff = 8
 vim.opt.signcolumn = "yes"
-vim.opt.isfname:append("@-@")
+vim.opt.isfname:append "@-@"
 vim.opt.updatetime = 1000
 
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
@@ -41,171 +40,329 @@ vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
 vim.keymap.set("n", "<C-d>", "<C-d>zz")
 vim.keymap.set("n", "<C-u>", "<C-u>zz")
 
---[[
-vim.opt.cursorline = true -- Highlight the current line
-local group = vim.api.nvim_create_augroup("CursorLineControl", { clear = true })
-local set_cursorline = function(event, value, pattern)
-  vim.api.nvim_create_autocmd(event, {
-    group = group,
-    pattern = pattern,
-    callback = function()
-      vim.opt_local.cursorline = value
-    end,
-  })
-end
-set_cursorline("WinLeave", false)
-set_cursorline("WinEnter", true)
-set_cursorline("FileType", false, "TelescopePrompt")
---]]
-
-local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+local lazypath = vim.fn.stdpath "data" .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
-  vim.fn.system({
+  vim.fn.system {
     "git",
     "clone",
     "--filter=blob:none",
     "https://github.com/folke/lazy.nvim.git",
     "--branch=stable",
     lazypath,
-  })
+  }
 end
 vim.opt.rtp:prepend(lazypath)
 
-local plugins = {
-    {
-        "ellisonleao/gruvbox.nvim",
-        priority = 1000,
-        config = function()
-            local gruvbox = require("gruvbox")
+require("lazy").setup({
+  {
+    "tpope/vim-sleuth",
+  },
+  {
+    "ellisonleao/gruvbox.nvim",
+    priority = 1000,
+    config = function()
+      local gruvbox = require "gruvbox"
 
-            gruvbox.setup({
-                terminal_colors = true,
-                undercurl = true,
-                underline = true,
-                bold = true,
-                italic = {
-                    strings = true,
-                    emphasis = true,
-                    comments = true,
-                    operators = false,
-                    folds = true,
-                },
-                strikethrough = true,
-                invert_selection = false,
-                invert_signs = false,
-                invert_tabline = false,
-                invert_intend_guides = false,
-                inverse = true,
-                contrast = "",
-                palette_overrides = {},
-                overrides = {},
-                dim_inactive = false,
-                transparent_mode = true
-            })
-
-            vim.cmd([[colorscheme gruvbox]])
-        end
-    },
-	{
-		"theprimeagen/harpoon",
-		dependencies = {
-			"nvim-lua/plenary.nvim"
-		},
-		config = function()
-			local mark = require("harpoon.mark")
-			local ui = require("harpoon.ui")
-			vim.keymap.set("n", "<leader>a", mark.add_file)
-			vim.keymap.set("n", "<C-e>", ui.toggle_quick_menu)
-			vim.keymap.set("n", "<C-h>", function() ui.nav_file(1) end)
-			vim.keymap.set("n", "<C-t>", function() ui.nav_file(2) end)
-			vim.keymap.set("n", "<C-n>", function() ui.nav_file(3) end)
-			vim.keymap.set("n", "<C-s>", function() ui.nav_file(4) end)
-        end
-	},
-	{
-        "nvim-telescope/telescope.nvim",
-        branch = "0.1.x",
-        dependencies = {
-            "nvim-lua/plenary.nvim",
+      gruvbox.setup {
+        terminal_colors = true,
+        undercurl = true,
+        underline = true,
+        bold = true,
+        italic = {
+          strings = true,
+          emphasis = true,
+          comments = true,
+          operators = false,
+          folds = true,
         },
-        config = function()
-            local telescope = require("telescope")
-            local actions = require("telescope.actions")
-            local builtin = require("telescope.builtin")
+        strikethrough = true,
+        invert_selection = false,
+        invert_signs = false,
+        invert_tabline = false,
+        invert_intend_guides = false,
+        inverse = true,
+        contrast = "",
+        palette_overrides = {},
+        overrides = {},
+        dim_inactive = false,
+        transparent_mode = true,
+      }
 
-            telescope.setup({
-                defaults = {
-                    path_display = {
-                        "truncate "
-                    },
-                    mappings = {
-                        i = {
-                            ["<C-k>"] = actions.move_selection_previous,
-                            ["<C-j>"] = actions.move_selection_next,
-                        },
-                    },
-                },
+      vim.cmd [[colorscheme gruvbox]]
+    end,
+  },
+  {
+    "theprimeagen/harpoon",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+    },
+    config = function()
+      local mark = require "harpoon.mark"
+      local ui = require "harpoon.ui"
+
+      vim.keymap.set("n", "<leader>a", mark.add_file)
+      vim.keymap.set("n", "<C-e>", ui.toggle_quick_menu)
+      vim.keymap.set("n", "<C-h>", function()
+        ui.nav_file(1)
+      end)
+      vim.keymap.set("n", "<C-t>", function()
+        ui.nav_file(2)
+      end)
+      vim.keymap.set("n", "<C-n>", function()
+        ui.nav_file(3)
+      end)
+      vim.keymap.set("n", "<C-s>", function()
+        ui.nav_file(4)
+      end)
+    end,
+  },
+  {
+    "nvim-telescope/telescope.nvim",
+    branch = "0.1.x",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+    },
+    config = function()
+      local telescope = require "telescope"
+      local actions = require "telescope.actions"
+      local builtin = require "telescope.builtin"
+
+      telescope.setup {
+        defaults = {
+          path_display = {
+            "truncate ",
+          },
+          mappings = {
+            i = {
+              ["<C-k>"] = actions.move_selection_previous,
+              ["<C-j>"] = actions.move_selection_next,
+            },
+          },
+        },
+      }
+
+      vim.keymap.set("n", "<leader>pf", builtin.find_files, {})
+      vim.keymap.set("n", "<C-p>", builtin.git_files, {})
+      vim.keymap.set("n", "<leader>ps", "<cmd>Telescope grep_string<cr>", {})
+    end,
+  },
+  {
+    "nvim-treesitter/nvim-treesitter",
+    build = {
+      ":TSUpdate",
+    },
+    config = function()
+      local configs = require "nvim-treesitter.configs"
+
+      configs.setup {
+        ensure_installed = {
+          "lua",
+          "php",
+          "javascript",
+          "jsdoc",
+          "typescript",
+          "bash",
+          "astro",
+          "yaml",
+          "svelte",
+          "regex",
+          "markdown",
+          "json",
+          "html",
+          "go",
+          "dockerfile",
+          "css",
+        },
+        highlight = {
+          enable = true,
+        },
+        indent = {
+          enable = true,
+        },
+      }
+    end,
+  },
+  {
+    "neovim/nvim-lspconfig",
+    dependencies = {
+      "williamboman/mason.nvim",
+      "williamboman/mason-lspconfig.nvim",
+      "WhoIsSethDaniel/mason-tool-installer.nvim",
+    },
+    config = function()
+      vim.api.nvim_create_autocmd("LspAttach", {
+        group = vim.api.nvim_create_augroup("kickstart-lsp-attach", { clear = true }),
+        callback = function(event)
+          local map = function(keys, func, desc)
+            vim.keymap.set("n", keys, func, { buffer = event.buf, desc = "LSP: " .. desc })
+          end
+
+          map("gd", require("telescope.builtin").lsp_definitions, "[G]oto [D]efinition")
+          map("gr", require("telescope.builtin").lsp_references, "[G]oto [R]eferences")
+          map("gI", require("telescope.builtin").lsp_implementations, "[G]oto [I]mplementation")
+          map("<leader>D", require("telescope.builtin").lsp_type_definitions, "Type [D]efinition")
+          map("<leader>ds", require("telescope.builtin").lsp_document_symbols, "[D]ocument [S]ymbols")
+          map("<leader>ws", require("telescope.builtin").lsp_dynamic_workspace_symbols, "[W]orkspace [S]ymbols")
+          map("<leader>rn", vim.lsp.buf.rename, "[R]e[n]ame")
+          map("<leader>ca", vim.lsp.buf.code_action, "[C]ode [A]ction")
+          map("K", vim.lsp.buf.hover, "Hover Documentation")
+          map("gD", vim.lsp.buf.declaration, "[G]oto [D]eclaration")
+
+          local client = vim.lsp.get_client_by_id(event.data.client_id)
+          if client and client.server_capabilities.documentHighlightProvider then
+            vim.api.nvim_create_autocmd({ "CursorHold", "CursorHoldI" }, {
+              buffer = event.buf,
+              callback = vim.lsp.buf.document_highlight,
             })
 
-            vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
-            vim.keymap.set('n', '<C-p>', builtin.git_files, {})
-            vim.keymap.set("n", "<leader>ps", "<cmd>Telescope grep_string<cr>", {})
+            vim.api.nvim_create_autocmd({ "CursorMoved", "CursorMovedI" }, {
+              buffer = event.buf,
+              callback = vim.lsp.buf.clear_references,
+            })
+          end
         end,
-    },
-    {
-        "nvim-treesitter/nvim-treesitter",
-        build = {
-            ":TSUpdate"
+      })
+
+      local capabilities = vim.lsp.protocol.make_client_capabilities()
+      capabilities = vim.tbl_deep_extend("force", capabilities, require("cmp_nvim_lsp").default_capabilities())
+
+      local servers = {
+
+        lua_ls = {
+          settings = {
+            Lua = {
+              runtime = { version = "LuaJIT" },
+              workspace = {
+                checkThirdParty = false,
+                library = {
+                  "${3rd}/luv/library",
+                  unpack(vim.api.nvim_get_runtime_file("", true)),
+                },
+              },
+              completion = {
+                callSnippet = "Replace",
+              },
+              diagnostics = {
+                disable = {
+                  "missing-fields",
+                },
+              },
+            },
+          },
         },
-        config = function ()
-            local configs = require("nvim-treesitter.configs")
-            
-            configs.setup({
-                ensure_installed = {
-                    "lua",
-                    "php",
-                    "javascript",
-                    "jsdoc",
-                    "typescript",
-                    "bash",
-                    "astro",
-                    "yaml",
-                    "svelte",
-                    "regex",
-                    "markdown",
-                    "json",
-                    "html",
-                    "go",
-                    "dockerfile",
-                    "css",
-                },
-                highlight = {
-                    enable = true
-                },
-                indent = {
-                    enable = true
-                }
-            })
+      }
+
+      require("mason").setup()
+
+      local ensure_installed = vim.tbl_keys(servers or {})
+      vim.list_extend(ensure_installed, {
+        "stylua",
+      })
+
+      require("mason-tool-installer").setup {
+        ensure_installed = ensure_installed,
+      }
+
+      require("mason-lspconfig").setup {
+        handlers = {
+          function(server_name)
+            local server = servers[server_name] or {}
+            server.capabilities = vim.tbl_deep_extend("force", {}, capabilities, server.capabilities or {})
+            require("lspconfig")[server_name].setup(server)
+          end,
+        },
+      }
+    end,
+  },
+  {
+    "stevearc/conform.nvim",
+    opts = {
+      notify_on_error = false,
+      format_on_save = {
+        timeout_ms = 500,
+        lsp_fallback = true,
+      },
+      formatters_by_ft = {
+        lua = {
+          "stylua",
+        },
+        javascript = {
+          "prettierd",
+        },
+      },
+    },
+  },
+  {
+    "hrsh7th/nvim-cmp",
+    event = "InsertEnter",
+    {
+      "L3MON4D3/LuaSnip",
+      build = (function()
+        if vim.fn.has "win32" == 1 or vim.fn.executable "make" == 0 then
+          return
         end
+        return "make install_jsregexp"
+      end)(),
     },
-    {
-        "nvim-treesitter/nvim-treesitter-context"
-    },
-    {
-        "github/copilot.vim"
-    },
-    {
-        "numToStr/Comment.nvim",
-        config = true
-    },
-    {
-        "iamcco/markdown-preview.nvim",
-        cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
-        ft = { "markdown" },
-        build = function() vim.fn["mkdp#util#install"]() end,
-    }
-}
+    "saadparwaiz1/cmp_luasnip",
+    "hrsh7th/cmp-nvim-lsp",
+    "hrsh7th/cmp-path",
+    "rafamadriz/friendly-snippets",
+    config = function()
+      local cmp = require "cmp"
+      local luasnip = require "luasnip"
+      luasnip.config.setup {}
 
-local opts = {}
-
-require("lazy").setup(plugins, opts)
+      cmp.setup {
+        snippet = {
+          expand = function(args)
+            luasnip.lsp_expand(args.body)
+          end,
+        },
+        completion = { completeopt = "menu,menuone,noinsert" },
+        mapping = cmp.mapping.preset.insert {
+          ["<C-n>"] = cmp.mapping.select_next_item(),
+          ["<C-p>"] = cmp.mapping.select_prev_item(),
+          ["<C-y>"] = cmp.mapping.confirm { select = true },
+          ["<C-Space>"] = cmp.mapping.complete {},
+          ["<C-l>"] = cmp.mapping(function()
+            if luasnip.expand_or_locally_jumpable() then
+              luasnip.expand_or_jump()
+            end
+          end, { "i", "s" }),
+          ["<C-h>"] = cmp.mapping(function()
+            if luasnip.locally_jumpable(-1) then
+              luasnip.jump(-1)
+            end
+          end, { "i", "s" }),
+        },
+        sources = {
+          { name = "nvim_lsp" },
+          { name = "luasnip" },
+          { name = "path" },
+        },
+      }
+    end,
+  },
+  {
+    "github/copilot.vim",
+  },
+  {
+    "numToStr/Comment.nvim",
+    config = true,
+  },
+  {
+    "iamcco/markdown-preview.nvim",
+    cmd = {
+      "MarkdownPreviewToggle",
+      "MarkdownPreview",
+      "MarkdownPreviewStop",
+    },
+    ft = {
+      "markdown",
+    },
+    build = function()
+      vim.fn["mkdp#util#install"]()
+    end,
+  },
+}, {})
