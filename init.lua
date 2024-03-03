13ee96ec32a826847e8cd958ee0a74441d180084
mbarnesdev <martinbarnes.dev@gmail.com>
2024-03-03 14:47:10 +0000

    decrease highlight time on yank

--- init.lua
+++ init.lua
@@ -50,7 +50,7 @@ vim.keymap.set("n", "<", "<<_")
 vim.api.nvim_create_autocmd("TextYankPost", {
   group = vim.api.nvim_create_augroup("mbarnesdev-highlight-yank", { clear = true }),
   callback = function()
-    vim.highlight.on_yank()
+    vim.highlight.on_yank { timeout = 50 }
   end,
 })
 
