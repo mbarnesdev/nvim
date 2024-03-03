983f3372b49e5be77cc883f3fcee19d6dafbc8e5
mbarnesdev <martinbarnes.dev@gmail.com>
2024-03-03 18:43:29 +0000

    restructure lazy setup + turn off plugin update notification

--- init.lua
+++ init.lua
@@ -16,6 +16,15 @@ if not vim.loop.fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
-require("lazy").setup {
-  { import = "mbarnesdev.plugins" },
+local plugins_dir = "mbarnesdev.plugins"
+local lazy_config = {
+  checker = {
+    enabled = true,
+    notify = false,
+  },
+  change_detection = {
+    notify = false,
+  },
 }
+
+require("lazy").setup({ import = plugins_dir }, lazy_config)
