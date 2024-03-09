d184111c7c9ab4ee6afdc7e8c281c21b7ab2c4c8
mbarnesdev <martinbarnes.dev@gmail.com>
2024-03-09 17:36:04 +0000

    fix astro

--- lua/mbarnesdev/plugins/lsp.lua
+++ lua/mbarnesdev/plugins/lsp.lua
@@ -44,6 +44,8 @@ return {
       capabilities = vim.tbl_deep_extend("force", capabilities, require("cmp_nvim_lsp").default_capabilities())
 
       local servers = {
+        astro = {},
+        tsserver = {},
         lua_ls = {
           settings = {
             Lua = {
