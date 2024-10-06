01ca8b795823ba019b7f6e81bb75de4858953d04
mbarnesdev <martinbarnesweb@gmail.com>
2024-10-06 04:34:49 +0100

    feat(plugin): oil

---
+++ lua/mbarnesdev/plugins/oil.lua
@@ -0,0 +1,25 @@
+return {
+  "stevearc/oil.nvim",
+  config = function()
+    local oil = require "oil"
+
+    oil.setup {
+      default_file_explorer = true,
+      columns = {
+        "icon",
+      },
+      use_default_keymaps = false,
+      keymaps = {
+        ["<CR>"] = "actions.select",
+        ["-"] = "actions.parent",
+      },
+      skip_confirm_for_simple_edits = true,
+      view_options = {
+        show_hidden = true,
+      },
+    }
+
+    vim.keymap.set("n", "<leader>pv", "<cmd>Oil<CR>")
+  end,
+  opts = {},
+}
