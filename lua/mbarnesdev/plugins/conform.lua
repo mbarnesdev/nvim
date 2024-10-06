f43893c7c682d37aee7726e03bad7b150bb1521e
mbarnesdev <martinbarnesweb@gmail.com>
2024-10-06 16:37:18 +0100

    feat(plugin): conform

---
+++ lua/mbarnesdev/plugins/conform.lua
@@ -0,0 +1,20 @@
+return {
+	"stevearc/conform.nvim",
+	config = function()
+		require("conform").setup({
+			formatters_by_ft = {
+				lua = { "stylua" },
+			},
+			format_on_save = {
+				timeout_ms = 500,
+			},
+		})
+
+		vim.api.nvim_create_autocmd("BufWritePre", {
+			pattern = "*",
+			callback = function(args)
+				require("conform").format({ bufnr = args.buf })
+			end,
+		})
+	end,
+}
