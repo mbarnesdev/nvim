71603da124094e8e13761d3efcedaa584cde5e39
mbarnesdev <martinbarnesweb@gmail.com>
2024-10-06 04:37:00 +0100

    chore: cleanup comments

--- init.lua
+++ init.lua
@@ -3,7 +3,6 @@ require "mbarnesdev.config.globals"
 require "mbarnesdev.config.autocmds"
 require "mbarnesdev.config.keymaps"
 
--- Bootstrap lazy.nvim
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
   local lazyrepo = "https://github.com/folke/lazy.nvim.git"
@@ -20,22 +19,12 @@ if not (vim.uv or vim.loop).fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
--- Make sure to setup `mapleader` and `maplocalleader` before
--- loading lazy.nvim so that mappings are correct.
--- This is also a good place to setup other settings (vim.opt)
-vim.g.mapleader = " "
-vim.g.maplocalleader = "\\"
-
--- Setup lazy.nvim
-require("lazy").setup({
-  spec = {
-    -- import your plugins
-    { import = "mbarnesdev.plugins" },
+require("lazy").setup({ import = "mbarnesdev.plugins" }, {
+  checker = {
+    enabled = true,
+    notify = false,
   },
-  -- Configure any other settings here. See the documentation for more details.
-  -- colorscheme that will be used when installing plugins.
-  install = { colorscheme = { "habamax" } },
-  -- automatically check for plugin updates
-  checker = { enabled = true },
+  change_detection = {
+    notify = false,
+  },
 })
-
