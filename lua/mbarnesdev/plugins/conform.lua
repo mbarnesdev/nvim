30ce793ecb417c2b05d4e8148be6d3096a1de513
mbarnesdev <martinbarnesweb@gmail.com>
2024-10-06 19:12:49 +0100

    feat(plugin): formatters for react

--- lua/mbarnesdev/plugins/conform.lua
+++ lua/mbarnesdev/plugins/conform.lua
@@ -5,6 +5,12 @@ return {
 			formatters_by_ft = {
 				lua = { "stylua" },
 				go = { "gofmt" },
+				html = { "prettier" },
+				json = { "prettier" },
+				javascript = { "prettier", stop_after_first = true },
+				javascriptreact = { "prettier", stop_after_first = true },
+				typescript = { "prettier", stop_after_first = true },
+				typescriptreact = { "prettier", stop_after_first = true },
 			},
 			format_on_save = {
 				timeout_ms = 500,
