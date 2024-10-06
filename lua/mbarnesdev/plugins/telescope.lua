7f37006cd4c545d70bf2a8713221445ed8063645
mbarnesdev <martinbarnesweb@gmail.com>
2024-10-06 22:37:05 +0100

    feat(plugin): telescope for ui select

--- lua/mbarnesdev/plugins/telescope.lua
+++ lua/mbarnesdev/plugins/telescope.lua
@@ -1,23 +1,35 @@
 return {
-	"nvim-telescope/telescope.nvim",
-	branch = "0.1.x",
-	dependencies = { "nvim-lua/plenary.nvim" },
-	config = function()
-		local actions = require("telescope.actions")
+	{
+		"nvim-telescope/telescope.nvim",
+		branch = "0.1.x",
+		dependencies = { "nvim-lua/plenary.nvim" },
+		config = function()
+			local actions = require("telescope.actions")
 
-		require("telescope").setup({
-			defaults = {
-				mappings = {
-					i = {
-						["<C-k>"] = actions.move_selection_previous,
-						["<C-j>"] = actions.move_selection_next,
+			require("telescope").setup({
+				defaults = {
+					mappings = {
+						i = {
+							["<C-k>"] = actions.move_selection_previous,
+							["<C-j>"] = actions.move_selection_next,
+						},
 					},
 				},
-			},
-		})
+				extensions = {
+					["ui-select"] = {
+						require("telescope.themes").get_dropdown({}),
+					},
+				},
+			})
 
-		vim.keymap.set("n", "<leader>pf", "<cmd>Telescope find_files<CR>")
-		vim.keymap.set("n", "<C-p>", "<cmd>Telescope git_files<CR>")
-		vim.keymap.set("n", "<leader>ph", "<cmd>Telescope help_tags<CR>")
-	end,
+			require("telescope").load_extension("ui-select")
+
+			vim.keymap.set("n", "<leader>pf", "<cmd>Telescope find_files<CR>")
+			vim.keymap.set("n", "<C-p>", "<cmd>Telescope git_files<CR>")
+			vim.keymap.set("n", "<leader>ph", "<cmd>Telescope help_tags<CR>")
+		end,
+	},
+	{
+		"nvim-telescope/telescope-ui-select.nvim",
+	},
 }
