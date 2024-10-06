ea61bff41c27c96e7d46cbb4d2622ff649596db4
mbarnesdev <martinbarnesweb@gmail.com>
2024-10-06 20:47:45 +0100

    feat(plugin): add transparency to theme

--- lua/mbarnesdev/plugins/theme.lua
+++ lua/mbarnesdev/plugins/theme.lua
@@ -4,5 +4,13 @@ return {
 	priority = 1000,
 	config = function()
 		vim.cmd.colorscheme("gruvbox-minor")
+
+		vim.api.nvim_set_hl(0, "Normal", { bg = "NONE" })
+		vim.api.nvim_set_hl(0, "NormalFloat", { bg = "NONE" })
+		vim.api.nvim_set_hl(0, "SignColumn", { bg = "NONE" })
+		vim.api.nvim_set_hl(0, "EndOfBuffer", { bg = "NONE" })
+		vim.api.nvim_set_hl(0, "LineNr", { bg = "NONE" })
+		vim.api.nvim_set_hl(0, "CursorLine", { bg = "NONE" })
+		vim.api.nvim_set_hl(0, "CursorLineNr", { bg = "NONE" })
 	end,
 }
