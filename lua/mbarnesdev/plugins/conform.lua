7e42105c02d07872466aa6c5fe78e6ec6fcbad7a
mbarnesdev <martinbarnesweb@gmail.com>
2024-10-06 18:40:19 +0100

    feat(plugin): lsp keybinds

--- lua/mbarnesdev/plugins/conform.lua
+++ lua/mbarnesdev/plugins/conform.lua
@@ -4,6 +4,7 @@ return {
 		require("conform").setup({
 			formatters_by_ft = {
 				lua = { "stylua" },
+				go = { "gofmt" },
 			},
 			format_on_save = {
 				timeout_ms = 500,
--- lua/mbarnesdev/plugins/lsp.lua
+++ lua/mbarnesdev/plugins/lsp.lua
@@ -7,14 +7,11 @@ return {
 	},
 	{
 		"williamboman/mason-lspconfig.nvim",
-		dependencies = {
-			"williamboman/mason.nvim",
-			"neovim/nvim-lspconfig",
-		},
 		config = function()
 			require("mason-lspconfig").setup({
 				ensure_installed = {
 					"lua_ls",
+					"gopls",
 				},
 				automatic_installation = true,
 			})
@@ -25,12 +22,34 @@ return {
 		config = function()
 			local lspconfig = require("lspconfig")
 
+			local on_attach = function(_, bufnr)
+				local function buf_set_keymap(...)
+					vim.api.nvim_buf_set_keymap(bufnr, ...)
+				end
+				local function buf_set_option(...)
+					vim.api.nvim_buf_set_option(bufnr, ...)
+				end
+
+				buf_set_option("omnifunc", "v:lua.vim.lsp.omnifunc")
+
+				local opts = { noremap = true, silent = true }
+				buf_set_keymap("n", "gd", "<cmd>lua vim.lsp.buf.definition()<CR>", opts)
+				buf_set_keymap("n", "rn", "<cmd>lua vim.lsp.buf.rename()<CR>", opts)
+			end
+
 			lspconfig.lua_ls.setup({
+				on_attach = on_attach,
 				settings = {
 					Lua = {
-						diagnostics = { globals = { "vim" } },
+						diagnostics = {
+							globals = { "vim" },
+						},
 					},
 				},
+			})
+
+			lspconfig.gopls.setup({
+				on_attach = on_attach,
 			})
 		end,
 	},
